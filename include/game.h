f10b7ad6ab5d74708a082d640a6d4a2f21646a5a
Adam Solloway <ajsolloway@gmail.com>
2023-02-02 00:11:43 -0500

    Adding GameList and search_games

--- include/game.h
+++ include/game.h
@@ -9,6 +9,18 @@ typedef struct Game {
     Channel *channels;
 } Game;
 
+// struct used for containing list of Games from Twitch queries
+// contains query string to ensure consecutive queries are valid
+typedef struct GameList {
+    Paginator iterator;
+    Game *games;
+    const char *query;
+    int len;
+} GameList;
+
 int get_top_games(Client *client, Game **games, Paginator *iterator, int items);
 int get_game_streams(Client *client, TwitchStream **streams, Game *from, Paginator *iterator, int items);
 void get_game_by_name(Client *client, char *name, Game *game);
+void search_games(Client *client, GameList *games);
+void GameList_init(GameList *games, const char *query);
+void GameList_deinit(GameList *games);
--- src/game.c
+++ src/game.c
@@ -61,6 +61,17 @@ int get_game_streams(Client *client, TwitchStream **streams, Game *from, Paginat
     return ret;
 }
 
+void GameList_init(GameList *games, const char *query) {
+    games->iterator = paginator_init();
+    games->games = NULL;
+    games->len = 0;
+    games->query = query;
+}
+
+void GameList_deinit(GameList *games) {
+    free(games->games);
+}
+
 void get_game_by_name(Client *client, char *name, Game *game) {
     cstr url = client->__url;
     cstrUpdateString(url, "https://api.twitch.tv/helix/games");
@@ -70,5 +81,39 @@ void get_game_by_name(Client *client, char *name, Game *game) {
     get_json_array(response, "data");
     json_object *json = json_object_array_get_idx(response->data, 0);
     init_game(game, json);
+    response_clean(response);
+}
+
+void search_games(Client *client, GameList *games) {
+    cstr url = client->__url;
+    int game_index = games->len;
+    cstrUpdateString(url, "https://api.twitch.tv/helix/search/categories?first=100");
+    if (games->iterator.pagination[0] != '\0') {
+        cstrCatFmt(url, "&after=%s", games->iterator.pagination);
+    }
+    cstrCatFmt(url, "&query=%s", games->query);
+    cstrReplace(url, " ", "%20");
+    Response *response = curl_request(client, url->string, curl_GET);
+    get_json_array(response, "data");
+    if (!games->games) {
+        games->games = malloc(sizeof(Game) * response->data_len);
+    } else {
+        games->games = realloc(games->games, sizeof(Game) * (response->data_len + games->len));
+    }
+    for (int i = 0; i < response->data_len; i++) {
+        Game game;
+        json_object *data_array_object;
+        data_array_object = json_object_array_get_idx(response->data, i);
+        const char *name = get_key(data_array_object, "name");
+        const char *id = get_key(data_array_object, "id");
+        const char *box_art_url = get_key(data_array_object, "box_art_url");
+        memcpy(game.name, name, strlen(name) + 1);
+        memcpy(game.id, id, strlen(id) + 1);
+        memcpy(game.box_art_url, box_art_url, strlen(box_art_url) + 1);
+        games->games[game_index] = game;
+        game_index++;
+    }
+    games->len += response->data_len;
+    paginator_set(games->iterator.pagination, response->response);
     response_clean(response);
 }
